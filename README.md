7bec9c2e5076d1ad20f909f838eb4cb85a3cc20f
Jcassdev <Jcass.dev@gmail.com>
2022-11-18 15:35:19 -0500

    Added Eventlisteners updated RPS to have buttons and even listeners

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # rock-paper-scissors
 Rock Paper Scissors
+Added Event Listeners.
--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,88 +1,79 @@
-/*Rock paper scissors is a game played by 2 people or 
-1 person vs a computer. Each player chooses either a 
-rock, a paper, or scissors and compares their choice
-to what the other player chose. Rock beats scissors.
-Scissors beats paper. Paper beats rock. Usually the 
-game is played best 2 out of 3 but for this assignment
-the game will go 5 rounds. */
+let playerWin = 0;
+let computerWin = 0;
+const playerScore = document.querySelector("#playerScore");
+const computerScore = document.querySelector("#computerScore");
+const content = document.querySelector("#content");
 
-//Create a variable that will store player guess data
-//Create 3 variables, rock, paper, scissors
-//Player 1 chooses a variable, either rock, scissors, or paper.
-let win = 0;
-let loss = 0;
-let tie = 0;
-for (let i = 0; i < 5; i++) {
-function playerSelection() {
-    let guess = prompt("Choose rock, paper, or scissors:").toLowerCase();
-    let answer = guess;
-    if (answer === 'rock') {
-        answer = 1;
-        console.log("You chose rock!");
-    } else if (answer === 'paper') {
-        answer = 2;
-        console.log("You chose paper!");
-    } else if (answer === 'scissors') {
-        answer = 3;
-        console.log("You chose scissors!");
-    } else {
-        console.log("You must choose rock, paper, or scissors!");
+const compSelection = document.createElement('div');
+compSelection.classList.add('compselection');
+const roundWinner = document.createElement('div');
+roundWinner.classList.add('roundwinner');
+const gameWinner = document.createElement('div');
+gameWinner.classList.add('gamewinner');
+ 
+
+computerScore.textContent = computerWin;
+playerScore.textContent = playerWin;
+
+const game = document.querySelector('.game');
+
+function playGame(e){
+    const { target } = (e);
+    let playerChoice = target.value;
+    if (!target.matches('button')) {
+        return;
     }
-    playerSelection = answer;
-}
-console.log(playerSelection());
-//Create a variable that will store computer guess data
-//Player 2 chooses a variable at random, """
-let randomNumber = Math.floor(Math.random() * 3) + 1; 
-function computerSelection() {
-    let getComputerChoice = randomNumber;
-    if (getComputerChoice === 1) {
-        console.log("The computer chose rock!");
-    } else if (getComputerChoice === 2) {
-        console.log("The computer chose paper!");
-    } else if (getComputerChoice === 3) {
-        console.log("The computer chose scissors!");
-    } else {
-        console.log("The computer didn't make a selection?");
+    let randomNumber = Math.floor(Math.random() * 3) + 1; 
+    function computerSelection() {
+        let getComputerChoice = randomNumber;
+        if (getComputerChoice === 1) {
+            compSelection.textContent = "The computer chose rock!";
+        } else if (getComputerChoice === 2) {
+            compSelection.textContent= "The computer chose paper!";
+        } else if (getComputerChoice === 3) {
+            compSelection.textContent = "The computer chose scissors!";
+        }
+        computerSelection = getComputerChoice;
+        content.append(compSelection);
     }
-    computerSelection = getComputerChoice;
-}
-console.log(computerSelection());
-//The chosen variables are compared.
-/*Scissor "3" is greater than rock "1" so scissor is beating
-rock. I need to make rock beat scissors. */
-function comparison() {
-    if (computerSelection === 1 && playerSelection === 3) {
-        console.log("The computer wins this round!");
-        loss+=1;
-    } else if (playerSelection === 1 && computerSelection === 3) {
-        console.log("You win this round!");
-        win+=1;
-    } else if (computerSelection > playerSelection) {
-        console.log("The computer wins this round!");
-        loss+=1;
-    } else if (computerSelection == playerSelection) {
-        console.log("It's a tie this round!");
-        tie+=1;
-    } else if (computerSelection < playerSelection) {
-        console.log("You win this round!");
-        win+=1;
-    } 
-}
-console.log(comparison());
-}
-if (win > loss) {
-    console.log("You win the game!");
-} else if (win < loss) {
-    console.log("You lose the game!");
-} else if (win === loss) {
-    console.log("Draw!");
-}
-
-
-/* The first two && objects were incorporated to fix the 
-scissors beating the rock*/
-
-//A winner for the round is chosen based on the compared variables.
-//The game ends after 5 rounds
-//A winner for the overall game is declared.
+    console.log(computerSelection())
+    function comparison() {
+        if (computerSelection === 1 && playerChoice == 3) {
+            computerWin+=1;
+            roundWinner.textContent = "The computer wins this round!";
+            
+        } else if (playerChoice == 1 && computerSelection === 3) {
+            playerWin+=1;
+            roundWinner.textContent = "You win this round!";
+                      
+        } else if (computerSelection > playerChoice) {
+            computerWin+=1;
+            roundWinner.textContent = "The computer wins this round!";
+               
+        } else if (computerSelection == playerChoice) {
+            roundWinner.textContent = "It's a tie this round!";
+            
+        } else if (computerSelection < playerChoice) {
+            playerWin+=1;
+            roundWinner.textContent = "You win this round!";
+    
+        } 
+        playerScore.innerHTML = playerWin;
+        computerScore.innerHTML = computerWin;
+        content.append(roundWinner);
+    };
+    console.log(comparison());
+    if (playerWin = 5) { 
+        
+        gameWinner.textContent = "You win the game!";
+        alert("Play again?");
+        document.location.reload();
+    } else if (computerWin = 5) {
+        
+        gameWinner.textContent = "You lose the game!";
+        alert("Play again?");
+        document.location.reload();
+    };
+    content.append(gameWinner);
+};
+game.addEventListener('click', playGame);
